2cdc75ab544653fb7322646309aa61585ef1102e
chenlong <826678202@qq.com>
2018-02-04 22:28:58 +0800

    this is a c language socket

---
+++ client.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORTNUM 6789
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    char buffer[1024];
+    struct sockaddr_in server_addr;
+    struct hostent *host;
+    int nbytes;
+
+    host = gethostbyname(argv[1]); //返回值不为NULL时正确
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;                         // IPV4
+    server_addr.sin_port = htons(PORTNUM);                    //
+    server_addr.sin_addr = *((struct in_addr *)host->h_addr); // IP地址
+
+    int ret = connect(sockfd, (struct sockaddr *)(&server_addr), sizeof(struct sockaddr));
+
+    while ((nbytes = read(sockfd, buffer, 1024)) != -1)
+    {
+        if (nbytes > 0)
+        {
+            buffer[nbytes] = '\0';
+            printf("client received:%s\n", buffer);
+            bzero(buffer, 1024);
+        }
+    }
+
+    close(sockfd);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORTNUM 6789
+#define MAX_SIZE 1024
+
+int main()
+{
+
+	int sockfd, new_fd;
+	struct sockaddr_in server_addr;
+	struct sockaddr_in client_addr;
+	socklen_t sin_size;
+	char msg[MAX_SIZE] = "Hello! ...\n";
+	//未做异常判断 socket() bind() listen() accept() wirte()错误返回值为-1
+
+	// AF_INET 表示IPV4版本 还有IPV6版本
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	printf("this type int desc:%d", sockfd);
+
+	bzero(&server_addr, sizeof(struct sockaddr_in));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY); //
+	printf("the server addr is:%x", INADDR_ANY);
+	//server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	server_addr.sin_port = htons(PORTNUM);
+
+	int ret = bind(sockfd, (struct sockaddr *)(&server_addr), sizeof(struct sockaddr));
+
+	ret = listen(sockfd, 5);
+
+	while (1)
+	{
+		sin_size = sizeof(server_addr);
+		new_fd = accept(sockfd, (struct sockaddr *)(&client_addr), &sin_size);
+		fprintf(stderr, "Server get connection from %s\n", inet_ntoa(client_addr.sin_addr));
+
+		ret = write(new_fd, msg, strlen(msg));
+		while (1)
+		{
+			bzero(msg, MAX_SIZE);
+			fgets(msg, MAX_SIZE, stdin);
+			msg[strlen(msg) - 1] = '\0';
+			printf("inputed: %s----input \"exit\" to quit\n", msg);
+			if (strcmp(msg, "exit") == 0)
+				break;
+			ret = write(new_fd, msg, strlen(msg));
+		}
+		close(new_fd);
+	}
+
+	close(sockfd);
+
+	return 0;
+}
